e308f858b462d6954133bc58bd7a73160e86d55b
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-07-31 12:26:34 +0200

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,7 +3,7 @@
 .onLoad <- function(libname, pkgname) {
   is_conda <- try(reticulate::conda_binary(), silent = TRUE)
   if(class(is_conda) == "try-error") {
-    warning("Anaconda not found on your computer. Conda related functionality such as create_env.R and condaenv and yml parameters from explain_scikitlearn will not be available")
+    message("Anaconda not found on your computer. Conda related functionality such as create_env.R and condaenv and yml parameters from explain_scikitlearn will not be available")
   }
 
 }
