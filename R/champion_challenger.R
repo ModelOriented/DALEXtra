8c6992556296041404a1f58a9ecfe2e42d54ca85
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-09-23 02:58:44 +0200

    Readme update

--- R/champion_challenger.R
+++ R/champion_challenger.R
@@ -1,5 +1,13 @@
 #' Compare machine learning models
 #'
+#' Determining if one model is better than the other one is a difficult task. Mostly because there is a lot of fields that have to be
+#' covered to make such a judgemnt. Overall performance, performance on the crucial subset, distribution of residuals, those are only
+#' few among many ideas related to that issue. Following function allow user to create a report based on various sections. Each says something different
+#' about relation between champion and challengers. \code{DALEXtra} package share 3 base sections which are \code{\link{funnel_measure}}
+#' \code{\link{overall_comparison}} and \code{\link{training_test_comparison}} but any object that has generic \code{plot} function can
+#' be inculded at report.
+#'
+#'
 #' @param sections - list of sections to be attached to report. Could be sections available with DALEXtra which are \code{\link{funnel_measure}}
 #' \code{\link{training_test_comparison}}, \code{\link{overall_comparison}} or any other explanation that can work with \code{plot} function.
 #' @param dot_size - dot_size argument passed to \code{\link{plot.funnel_measure}} if \code{\link{funnel_measure}} section present
