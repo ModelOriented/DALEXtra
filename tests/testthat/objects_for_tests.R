2f3e29eaad13baeeecd71e99da4e3faad58a5e41
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2019-10-29 17:54:16 +0100

    Update objects_for_tests.R

--- tests/testthat/objects_for_tests.R
+++ tests/testthat/objects_for_tests.R
@@ -1,7 +1,6 @@
 library(DALEX)
 library(reticulate)
 
-reticulate::use_condaenv("myenv")
 # helper function to skip tests if we don't have the 'shap' module
 skip_if_no_conda <- function() {
   tryCatch(conda_binary(),
